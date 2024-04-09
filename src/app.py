bfba76c47d14b1d7949e5923d4f554ef1c351f1d
aferyst z bolehova <formicking@gmail.com>
2024-04-09 23:02:01 +0300

    Reskin UI

--- src/app.py
+++ src/app.py
@@ -9,15 +9,9 @@ from ui_layout import *
 # define UI layout
 app_ui = ui.page_fillable(
     ui.card(
-        ui.card_header("FPV Drone Configurator"),
+        ui.card_header(ui.input_dark_mode(id="mode"), " FPV Drone Configurator"),
         ui.layout_columns(card_inputs, card_results),
-    ),
-    ui.card(
-        ui.card_header("Selection"),
-        ui.download_button("download_file", "Download Selected Properties"),
-        ui.input_action_button("action_button", "Show/Hide Configuration"),
-        ui.output_text_verbatim("configuration"),
-    ),
+    )
 )
 
 
@@ -26,26 +20,42 @@ def server(input, output, session):
 
     @reactive.effect
     def _():
-        if input.drop_servo() == True and input.camera_servo() == False:
-            ui.update_select("drop_servo_pad", choices=SERVO_PAD_CHOICES)
-        elif input.camera_servo() == True and input.drop_servo() == False:
-            ui.update_select("camera_servo_pad", choices=SERVO_PAD_CHOICES)
-        elif input.camera_servo() == True and input.drop_servo() == True:
-
+        # * If user needs 2 servos we should split available options
+        # * to prevent accidental selections of the same pad
+        # * I tried dynamic evaluation but id did not work well
+        if len(input.servo_settings()) == 2:
             ui.update_select(
                 "drop_servo_pad",
-                choices={k: SERVO_PAD_CHOICES[k] for k in list(SERVO_PAD_CHOICES)[:2]},
+                choices={k: SERVO_PAD_CHOICES[k] for k in list(SERVO_PAD_CHOICES)[:3]},
             )
             ui.update_select(
                 "camera_servo_pad",
-                choices={k: SERVO_PAD_CHOICES[k] for k in list(SERVO_PAD_CHOICES)[2:]},
+                choices={k: SERVO_PAD_CHOICES[k] for k in list(SERVO_PAD_CHOICES)[3:]},
             )
+        elif (
+            len(input.servo_settings()) == 1
+            and input.servo_settings()[0] == "drop_servo"
+        ):
+            ui.update_select("drop_servo_pad", choices=SERVO_PAD_CHOICES)
+        elif (
+            len(input.servo_settings()) == 1
+            and input.servo_settings()[0] == "camera_servo"
+        ):
+            ui.update_select("camera_servo_pad", choices=SERVO_PAD_CHOICES)
         else:
+
             ui.update_select("drop_servo_pad", choices={})
             ui.update_select("camera_servo_pad", choices={})
 
         ui.update_select("motor", choices=MOTOR_CHOICES[input.frame_size()])
         ui.update_select("frame", choices=FRAME_CHOICES[input.frame_size()])
+
+        if input.osd_options() == "vd":
+            ui.update_radio_buttons(id="version", choices={"43": "4.3"})
+        else:
+            ui.update_radio_buttons(
+                id="version", choices={"43": "4.3", "44": "4.4", "45": "4.5"}
+            )
 
     @output
     @render.download(
--- src/ui_layout.py
+++ src/ui_layout.py
@@ -1,17 +1,29 @@
 from shiny import ui
 
-SERVO_PAD_CHOICES = {"S9": "S9", "M8": "M8", "M5": "M5", "M4": "M4"}
+# https://icons.getbootstrap.com/icons/question-circle-fill/
+question_circle_fill = ui.HTML(
+    '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-question-circle-fill mb-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>'
+)
+
+SERVO_PAD_CHOICES = {
+    "S9": "S9",
+    "M8": "M8",
+    "M7": "M7",
+    "M6": "M6",
+    "M5": "M5",
+    "M4": "M4",
+}
 UART_CHOICES = {"2": "UART 2", "6": "UART 6"}
 MOTOR_CHOICES = {
     "S": {
-        "fh2807": "Flashhobby 2807 1300KV",
         "em2807": "Emax Eco II 2807 1300KV",
+        "fh2807": "Flashhobby 2807 1300KV",
         "tm2806": "T-Motor F90 2806.5 1300KV",
     },
     "M": {"bh2810": "Brotherhobby Avenger 2810 900KV"},
     "L": {
         "xc3110": "XING2 Cinelifter 3110 1250KV",
-        "bh3212": "BrotherHobby Special Edition V4 32.5 12 950KV",
+        "bh3212": "BrotherHobby Special Edition V4 32.5 12 95KV",
     },
 }
 
@@ -34,7 +46,7 @@ FRAME_CHOICES = {
 
 card_results = ui.card(
     ui.card_header("Selection results"),
-    ui.output_plot("loss_over_time"),
+    ui.output_text_verbatim(id="selection_results"),
     full_screen=True,
 )
 
@@ -43,44 +55,49 @@ card_inputs = ui.card(
     ui.row(
         ui.column(
             4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Select frame size:"),
+                "Frame size will limit motors and frame selection according to recommended setups",
+                placement="right",
+            ),
             ui.input_radio_buttons(
                 id="frame_size",
-                label="Frame frame_size (inches):",
+                label="",
                 choices={"S": "7 inch", "M": "8 inch", "L": "10 inch"},
                 inline=True,
+                # width="95%",
+            ),
+        ),
+        ui.column(
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Servo motor settings:"),
+                "Select one or more servomotors and corresponding pads depending on your setup. In most cases these applicable for bombers",
+                placement="right",
+            ),
+            ui.input_checkbox_group(
+                id="servo_settings",
+                label="",
+                choices={
+                    "drop_servo": ui.span("Enable Drop Servo"),
+                    "camera_servo": ui.span("Enable Camera Servo"),
+                },
+                # width="100%",
             ),
         ),
         ui.column(
             2,
-            ui.input_switch(
-                id="drop_servo",
-                label="Enable Drop Servo",
-            ),
-        ),
-        ui.column(
-            1,
             ui.input_select(
-                id="drop_servo_pad",
-                label="Servo Pad:",
-                choices={},
+                id="drop_servo_pad", label="Drop Servo Pad:", choices={}, width="95%"
             ),
-        ),
-        ui.column(
-            1,
         ),
         ui.column(
             2,
-            ui.input_switch(
-                id="camera_servo",
-                label="Enable Camera Servo",
-            ),
-        ),
-        ui.column(
-            1,
             ui.input_select(
                 id="camera_servo_pad",
-                label="Servo Pad:",
+                label="Camera Servo Pad:",
                 choices={},
+                # width="90%",
             ),
         ),
     ),
@@ -91,6 +108,7 @@ card_inputs = ui.card(
                 id="frame",
                 label="Select Frame type:",
                 choices={},
+                # width="95%"
             ),
         ),
         ui.column(
@@ -99,6 +117,26 @@ card_inputs = ui.card(
                 id="motor",
                 label="Select Motors type:",
                 choices={},
+                # width="95%"
+            ),
+        ),
+        ui.column(
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " DSHOT configuration:"),
+                "You must flash your ESC with Bluejay firmware before enabling this option",
+                placement="right",
+            ),
+            ui.input_select(
+                id="dshot",
+                label="",
+                choices={
+                    "none": "None",
+                    "d150": "D150",
+                    "d300": "D300",
+                    "d600": "D600",
+                },
+                # width="95%",
             ),
         ),
     ),
@@ -115,15 +153,39 @@ card_inputs = ui.card(
                     "vyriy": "Vyriy Drone + Q656 ESC",
                     "dmf722": "Diatone mamba F722",
                 },
+                # width="95%",
             ),
         ),
         ui.column(
             4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Betaflight version:"),
+                "Firmware version migh be not compatible with provided presets, e.g VD OSD settings. I did my best to prevent tool from misconfiguration just raise Github issue if you find some errors",
+                placement="right",
+            ),
             ui.input_radio_buttons(
                 id="version",
                 label="Betaflight version",
                 choices={"43": "4.3", "44": "4.4", "45": "4.5"},
                 inline=True,
+                # width="100%",
+            ),
+        ),
+        ui.column(
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Stack settings:"),
+                "'Blackbox' migh not work properly at some stacks and should be used only if you need that. 'Apply default settings' will apply switch and adjustment ranges.",
+                placement="right",
+            ),
+            ui.input_checkbox_group(
+                id="stack_settings",
+                label="Stack settings:",
+                choices={
+                    "blackbox": ui.span("Enable blackbox"),
+                    "defaults": ui.span("Apply default settings"),
+                },
+                # width="95%",
             ),
         ),
     ),
@@ -146,28 +208,92 @@ card_inputs = ui.card(
                     "gmp40": "GEPRC Maten Pro 2.5W 40CH 5.8G",
                     "blw40": "BLITZ Whoop 2.5W 40CH 5.8G",
                 },
+                # width="95%",
             ),
         ),
         ui.column(
-            2, ui.input_checkbox(id="set_vtx_power", label="Set VTX Power on switch?")
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " VTX settings:"),
+                "VTX power on switch will allow pilot to set power level during the fly. PIT mode will keep the power on miminal value until drone Arm to prevent overheating. Migh be unavailable on some transmitters",
+                placement="right",
+            ),
+            ui.input_checkbox_group(
+                id="vtx_settings",
+                label="VTX settings:",
+                choices={
+                    "set_vtx_power": ui.span("Set VTX Power on switch"),
+                    "enable_pit_mode": ui.span("Enable PIT mode"),
+                },
+                # width="95%",
+            ),
         ),
-        ui.column(2, ui.input_checkbox(id="enable_pit_mode", label="Enable PIT mode?")),
     ),
     ui.row(
         ui.column(
-            2,
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Change RX UART:"),
+                "In case your RX was set to different UART you maust change it here. Otherwise, leave default selection",
+                placement="right",
+            ),
             ui.input_select(
                 id="rx_uart",
-                label="Change RX UART:",
+                label="",
                 choices=UART_CHOICES,
+                # width="95%"
             ),
         ),
-        ui.column(2, ui.input_checkbox(id="set_pids", label="Apply PIDs")),
-        ui.column(2, ui.input_checkbox(id="set_filters", label="Apply Filters")),
-        ui.column(2, ui.input_checkbox(id="set_osd", label="Apply OSD settings")),
         ui.column(
-            2, ui.input_checkbox(id="set_beeper", label="Set beeper on Failsafe")
+            4,
+            ui.tooltip(
+                ui.span(question_circle_fill, " Additional settings:"),
+                "PIDs and Filters will be applied according to selected setup. If not checked, default values will be used.",
+                placement="right",
+            ),
+            ui.input_checkbox_group(
+                id="additional_settings",
+                label="",
+                choices={
+                    "set_pids": ui.span("Apply PIDs"),
+                    "set_filters": ui.span("Apply Filters"),
+                    "set_beeper": ui.span("Set beeper on Failsafe"),
+                },
+                # width="100%",
+            ),
         ),
+        ui.column(
+            4,
+            ui.input_select(
+                id="osd_options",
+                label="Select OSD style:",
+                choices={
+                    "vd": "Victory Drones",
+                    "io": "Victory Drones(Ovcharyk Edition)",
+                    "none": "Default(No preset)",
+                },
+                # width="95%",
+            ),
+        ),
+        ui.row(
+            ui.column(
+                4,
+                ui.HTML(
+                    '<a href="https://docs.google.com/spreadsheets/d/1rPLRIYMsm0Saati5lKZGK8HmkwtcOR2byXhdLkJrL_E/edit#gid=31662901" target="_blank">Check recommended drone parts</a>'
+                ),
+            ),
+            ui.column(
+                4,
+                ui.HTML(
+                    '<a href="https://github.com/formicking/fvp-cli-configurator" target="_blank">Check tool documentation</a>'
+                ),
+            ),
+        ),
+    ),
+    ui.card_footer(
+        ui.input_action_button(id="action_button", label="Confirm Configuration"),
+        ui.download_button("download_file", "Download Selected Properties"),
+        ui.input_action_button(id="reset", label="Clear Output"),
     ),
     full_screen=True,
 )
